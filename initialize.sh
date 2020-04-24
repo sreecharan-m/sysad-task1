18cdf0f3e713a513284e6a98295c093509bb200a
sreecharan-m <63252117+sreecharan-m@users.noreply.github.com>
2020-04-24 12:32:25 +0530

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -9,11 +9,11 @@ alias userGenerate='bash ./subtask1.sh'
 alias permit='bash ./subtask2.sh'
 alias autoSchedule='bash ./subtask3.sh'
 alias attendance='bash ./subtask4.sh'
-alias record1='bash /home/ArmyGeneral/./subtask51.sh'
-alias record2='bash /home/NavyMarshal/./subtask52.sh'
-alias record3='bash /home/AirforceChief/./subtask52.sh'
-alias finalattendance='bash /home/ChiefCommander/./hacker1.sh'
-alias nearest='bash /home/ChiefCommander/./hacker2.sh'
+alias record1='bash /home/ArmyGeneral/subtask51.sh'
+alias record2='bash /home/NavyMarshal/subtask52.sh'
+alias record3='bash /home/AirforceChief/subtask52.sh'
+alias finalattendance='bash /home/ChiefCommander/hacker1.sh'
+alias nearest='bash /home/ChiefCommander/hacker2.sh'
 
 wget https://inductions.delta.nitt.edu/sysad-task1-position.log
 wget https://inductions.delta.nitt.edu/sysad-task1-attendance.log
